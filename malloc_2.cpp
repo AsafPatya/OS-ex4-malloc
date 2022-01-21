e798d6814fd46246977b25f99f50371b9e748f49
student <asafpatya1994@gmail.com>
2022-01-21 20:15:56 +0000

    change name from 44 to the end

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,4 +1,3 @@
-
 #include <unistd.h>
 #include <stdio.h>
 #include <cstring>
@@ -9,24 +8,24 @@
 class MallocMetadata {
     size_t size;
     bool is_free;
-    void *address;
     MallocMetadata *next;
     MallocMetadata *prev;
+    void *address;
 
-    // constractor for empty list
+    // constructor for empty list
     MallocMetadata(size_t size, void *addr):size(size), is_free(false), address(addr)
     {
         this->next= nullptr;
         this->prev= nullptr;
     }
 
-    // constractor for nonempty
+    // constructor for nonempty
     MallocMetadata(size_t size, void *addr, MallocMetadata *prev) :
     size(size), is_free(false), address(addr) {
         this->prev=prev;
         this->next=next;
     }
-    //so we can use its privete staff in list
+    //so we can use its private staff in list
     friend class List;
 };
 
@@ -43,9 +42,8 @@ class List {
     }
 
 public:
-    //ctor
     List() : head(nullptr), tail(nullptr) {}
-    void *Insert(size_t size)
+    void *inset_to_mem_allocation_list(size_t size)
     {
         //case empty list
         if (head == nullptr)
@@ -97,7 +95,7 @@ public:
         }
     }
 
-    void set_free_block(void *addr)
+    void mem_alloc_list_set_block_to_free_by_address(void *addr)
     {
         /// function called from functions:
         /// number 3 -> sfree, number 4-> srealloc
@@ -106,20 +104,20 @@ public:
         {
             return;
         }
-        MallocMetadata *curr = head;
-        while (curr)
+        MallocMetadata *current_block = head;
+        while (current_block)
         {
             ///  Releases the usage of the block that starts with the pointer ‘p’
             /// so need to check !curr->is_free
-            if (curr->address == addr && !curr->is_free) {
-                curr->is_free = true;
+            if (current_block->address == addr && !current_block->is_free) {
+                current_block->is_free = true;
                 return;
             }
-            curr = curr->next;
+            current_block = current_block->next;
         }
     }
 
-    void *get_addr_of_block_by_size(size_t size)
+    void *mem_alloc_list_get_address_of_block_by_size(size_t size)
     {
         /// called from function number 1 -> smalloc
         if (head == nullptr)
@@ -127,112 +125,113 @@ public:
             // the list is empty, no free blocks
             return nullptr;
         }
-        MallocMetadata *curr = head;
-        while (curr)
+        MallocMetadata *current_block = head;
+        while (current_block)
         {
-            if (curr->is_free && curr->size >= size)
+            if (current_block->is_free && current_block->size >= size)
             {
-                curr->is_free = false;
-                return curr->address;
+                current_block->is_free = false;
+                return current_block->address;
             }
-            curr = curr->next;
+            current_block = current_block->next;
         }
         // not found free block have to allocate
         return nullptr;
     }
 
-    size_t get_block_size_by_addr(void *addr)
+    size_t mem_alloc_list_get_block_size_by_address(void *addr)
     {
         /// used in function number 4 -> srealloc
         if (head == nullptr)
         {
             return 0;
         }
-        MallocMetadata *curr = head;
-        while (curr)
+        MallocMetadata *current_block = head;
+        while (current_block)
         {
-            if (curr->address == addr)
+            if (current_block->address == addr)
             {
-                return curr->size;
+                return current_block->size;
             }
-            curr = curr->next;
+            current_block = current_block->next;
         }
         return 0;
     }
 
 
-    size_t get_num_free_blocks()
+    size_t mem_alloc_list_get_number_of_free_blocks()
     {
         /// called from function number 5 -> _num_free_blocks
-        size_t count = 0;
-        MallocMetadata *curr = head;
-        while (curr)
+        size_t number_of_free_blocks_counter = 0;
+        MallocMetadata *current_block = head;
+        while (current_block)
         {
-            if (curr->is_free)
+            if (current_block->is_free)
             {
-                count++;
+                number_of_free_blocks_counter++;
             }
-            curr = curr->next;
+            current_block = current_block->next;
         }
-        return count;
+        return number_of_free_blocks_counter;
     }
 
-    size_t get_num_free_bytes()
+    size_t mem_alloc_list_get_num_free_bytes()
     {
         /// called from function number 6 -> _num_free_bytes
-        size_t sum = 0;
-        MallocMetadata *curr = head;
-        while (curr)
+        size_t num_free_bytes_counter = 0;
+        MallocMetadata *current_block = head;
+        while (current_block)
         {
-            if (curr->is_free)
+            if (current_block->is_free)
             {
-                sum += curr->size;
+                num_free_bytes_counter += current_block->size;
             }
-            curr = curr->next;
+            current_block = current_block->next;
         }
-        return sum;
+        return num_free_bytes_counter;
     }
 
-    size_t get_num_allocated_blocks()
+    size_t mem_alloc_list_get_num_allocated_blocks()
     {
         /// called from function number 7 -> _num_allocated_blocks
-        size_t counter = 0;
-        MallocMetadata *curr = head;
-        while (curr)
+        size_t num_allocated_blocks_counter = 0;
+        MallocMetadata *current_block = head;
+        while (current_block)
         {
-            counter++;
-            curr = curr->next;
+            num_allocated_blocks_counter++;
+            current_block = current_block->next;
         }
-        return counter;
+        return num_allocated_blocks_counter;
     }
 
-    size_t get_num_allocated_bytes()
+    size_t mem_alloc_list_get_num_allocated_bytes()
     {
         /// called from function number 8 -> _num_allocated_bytes
-        size_t sum = 0;
-        MallocMetadata *curr = head;
-        while (curr) {
-            sum += curr->size;
-            curr = curr->next;
+        size_t num_allocated_bytes = 0;
+        MallocMetadata *current_block = head;
+        while (current_block) {
+            num_allocated_bytes += current_block->size;
+            current_block = current_block->next;
         }
-        return sum;
+        return num_allocated_bytes;
     }
 
-    size_t get_metadata_size()
+    size_t mem_alloc_list_get_metadata_size()
     {
         /// called from function number 10 -> _size_meta_data
         return sizeof(MallocMetadata);
     }
-    size_t get_num_metadata_bytes()
+    size_t mem_alloc_list_get_num_metadata_bytes()
     {
         /// called from function number 9 -> _num_meta_data_bytes
-        return get_num_allocated_blocks() * get_metadata_size();
+        return mem_alloc_list_get_num_allocated_blocks() * mem_alloc_list_get_metadata_size();
     }
 
 };
 
-List *malloc_block_list = (List *) sbrk(sizeof(List));
+List *malloc_memory_allocation_block_list = (List *) sbrk(sizeof(List));
 
+/// #smalloc
 void *smalloc(size_t size)
 {
     if (size == 0 || size > MAX_BLOCK_SIZE)
@@ -240,46 +239,50 @@ void *smalloc(size_t size)
         return nullptr;
     }
     /// Searches for a free block with up to ‘size’ bytes
-    void *_naddr = malloc_block_list->get_addr_of_block_by_size(size);
-    if (_naddr != nullptr)
+    void *address = malloc_memory_allocation_block_list->mem_alloc_list_get_address_of_block_by_size(size);
+    if (address != nullptr)
     {
-        return _naddr;
+        return address;
     }
 
     /// allocates (sbrk()) one if none are found.
-    void *ans = malloc_block_list->Insert(size);
-    if (ans == nullptr)
+    void *insert_result = malloc_memory_allocation_block_list->inset_to_mem_allocation_list(size);
+    if (insert_result == nullptr)
     {
         return nullptr;
     }
-    return ans;
+    return insert_result;
 }
 
+/// #scalloc
 void *scalloc(size_t num, size_t size)
 {
-    size_t _nsize = num*size;
+    size_t new_size = num * size;
 
     /// Searches for a free block of up to ‘num’ elements
-    void *addr = smalloc(_nsize);
+    void *address = smalloc(new_size);
 
-    if (addr == nullptr)
+    if (address == nullptr)
     {
         return nullptr;
     }
     // set blocks to 0
-    memset(addr, 0, _nsize);
-    return addr;
+    int zero = 0;
+    memset(address, zero, new_size);
+    return address;
 }
 
+/// #sfree
 void sfree(void *p)
 {
     /// If ‘p’ is NULL or already released, simply returns.
     if (p == nullptr) {
         return;
     }
-    malloc_block_list->set_free_block(p);
+    malloc_memory_allocation_block_list->mem_alloc_list_set_block_to_free_by_address(p);
 }
 
+///#sreaalloc
 void *srealloc(void *oldp, size_t size)
 {
     if (size == 0 || size > MAX_BLOCK_SIZE)
@@ -295,8 +298,8 @@ void *srealloc(void *oldp, size_t size)
     }
 
     /// If ‘size’ is smaller than the current block’s size, reuses the same block
-    size_t _nsize = malloc_block_list->get_block_size_by_addr(oldp);
-    if (_nsize >= size)
+    size_t new_size = malloc_memory_allocation_block_list->mem_alloc_list_get_block_size_by_address(oldp);
+    if (new_size >= size)
     {
         // block size match
         return oldp;
@@ -304,45 +307,45 @@ void *srealloc(void *oldp, size_t size)
 
     /// Otherwise, finds/allocates ‘size’ bytes for a new space, copies content of oldp into the new
     /// allocated space and frees the oldp.
-    void *_naddr = smalloc(size);
-    if (_naddr == nullptr)
+    void *address_from_smalloc = smalloc(size);
+    if (address_from_smalloc == nullptr)
     {
         return nullptr;
     }
 
-    memmove(_naddr, oldp, _nsize);
-    malloc_block_list->set_free_block(oldp);
-    return _naddr;
+    memmove(address_from_smalloc, oldp, new_size);
+    malloc_memory_allocation_block_list->mem_alloc_list_set_block_to_free_by_address(oldp);
+    return address_from_smalloc;
 }
 
 /// Returns the number of allocated blocks in the heap that are currently free
 size_t _num_free_blocks() {
-    return malloc_block_list->get_num_free_blocks();
+    return malloc_memory_allocation_block_list->mem_alloc_list_get_number_of_free_blocks();
 }
 
 /// Returns the number of bytes in all allocated blocks in the heap that are currently free,
 /// excluding the bytes used by the meta-data structs.
 size_t _num_free_bytes() {
-    return malloc_block_list->get_num_free_bytes();
+    return malloc_memory_allocation_block_list->mem_alloc_list_get_num_free_bytes();
 }
 
 /// Returns the overall (free and used) number of allocated blocks in the heap.
 size_t _num_allocated_blocks() {
-    return malloc_block_list->get_num_allocated_blocks();
+    return malloc_memory_allocation_block_list->mem_alloc_list_get_num_allocated_blocks();
 }
 
 /// Returns the overall number (free and used) of allocated bytes in the heap, excluding
 /// the bytes used by the meta-data structs.
 size_t _num_allocated_bytes() {
-    return malloc_block_list->get_num_allocated_bytes();
+    return malloc_memory_allocation_block_list->mem_alloc_list_get_num_allocated_bytes();
 }
 
 /// Returns the overall number of meta-data bytes currently in the heap.
 size_t _num_meta_data_bytes() {
-    return malloc_block_list->get_num_metadata_bytes();
+    return malloc_memory_allocation_block_list->mem_alloc_list_get_num_metadata_bytes();
 }
 
 // Returns the number of bytes of a single meta-data structure in your system.
 size_t _size_meta_data() {
-    return malloc_block_list->get_metadata_size();
+    return malloc_memory_allocation_block_list->mem_alloc_list_get_metadata_size();
 }
